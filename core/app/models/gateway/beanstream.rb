926011b39c348c289de52a3151e07afbd26bc834
John Hwang <johnyhwang@gmail.com>
2012-03-15 18:04:40 -0400

    Stripping out even more stuff

--- core/app/models/gateway/beanstream.rb
+++ core/app/models/gateway/beanstream.rb
@@ -31,27 +31,7 @@ class Gateway::Beanstream < Gateway
     end
   end
 
-  def capture(transaction, creditcard, gateway_options)
-    transaction = (transaction.amount * 100).round if payment_profiles_supported?
-    beanstream_gateway.capture(transaction, transaction.response_code, gateway_options)
-  end
-
-  def void(transaction_response, creditcard, gateway_options)
-    beanstream_gateway.void(transaction_response, gateway_options)
-  end
-
-  def credit(amount, creditcard, response_code, gateway_options = {})
-    amount = (amount * -1) if amount < 0
-    beanstream_gateway.credit(amount, response_code, gateway_options)
-  end
-
   private
-
-  def beanstream_gateway
-    ActiveMerchant::Billing::Base.gateway_mode = preferred_server.to_sym
-    gateway_options = options
-    ActiveMerchant::Billing::BeanstreamGateway.new(gateway_options)
-  end
 
   def verify_creditcard_name!(creditcard)
     bill_address = creditcard.payments.first.order.bill_address
